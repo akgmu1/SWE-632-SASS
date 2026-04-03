a2606e4bd0be3f2f229967aa3d8031028d1972e0
akgmu1 <113523061+akgmu1@users.noreply.github.com>
2026-04-03 16:20:55 -0400

    Mess around more with sass

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,10 +7,20 @@ const list = ref([1, 2, 3, 4, 5])
 <template>
   <main>
     <header>Hello</header>
-    <div class="items">
+    <div
+      class="items"
+      :style="{
+        'margin-bottom': '1rem',
+      }"
+    >
       <div v-for="x in list">
         <div class="item">{{ x }}</div>
       </div>
+    </div>
+    <div class="items">
+      <button class="btn btn-outline">Action A</button>
+      <button class="btn btn-primary">Action B</button>
+      <button class="btn btn-outline-primary">Action B</button>
     </div>
   </main>
 </template>
@@ -20,11 +30,13 @@ const list = ref([1, 2, 3, 4, 5])
 
 header {
   font-size: $font-2xl;
-  background-color: $primary;
-  color: $bg-color;
-  margin-bottom: rem(20);
-  padding-bottom: rem(10);
   text-align: center;
+
+  background-color: $primary-color;
+  color: $bg-color;
+
+  margin-bottom: 1rem;
+  padding-bottom: 0.5rem;
 }
 
 .items {
@@ -35,11 +47,12 @@ header {
 .item {
   background-color: $fg-color;
   color: $bg-color;
-  border-radius: rem(5);
+
+  @include round;
   padding: 0.5rem;
 
   &:hover {
-    background-color: $primary;
+    background-color: $primary-color;
     color: $bg-color;
   }
 }
