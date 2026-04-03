15d1e878e1e7573876a66429c0dbb2f3d0d083a6
akgmu1 <113523061+akgmu1@users.noreply.github.com>
2026-04-03 11:52:37 -0400

    Experiment

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const list = ref([1, 2, 3, 4, 5])
+</script>
 
 <template>
   <main>
-    <div class="test header">Hello</div>
+    <div class="header">Hello</div>
+    <div class="items">
+      <div v-for="x in list">
+        <div class="item">{{ x }}</div>
+      </div>
+    </div>
   </main>
 </template>
 
 <style lang="scss" scoped>
+@use '@/assets/_mixins.scss' as *;
+
 .header {
   font-size: 5rem;
-  text-align: center;
+}
+
+.items {
+  @include flex-center;
+  gap: 1rem;
+}
+
+.item {
+  font-size: 1rem;
+  background-color: blue;
+  color: white;
   border-radius: 1rem;
+  padding: 0.5rem;
+
+  &:hover {
+    background-color: aquamarine;
+  }
 }
 </style>
